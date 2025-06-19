9ee5c1f2675a6ba676411930f87824b608f2348f
tsents <tsents.sagie@gmail.com>
2025-06-19 11:33:35 +0300

    replace types to be unsigned.

--- primer.cpp
+++ primer.cpp
@@ -1,20 +1,27 @@
 #include "primer.h"
 
 
-bool primeMillerRabin(long long num, int accaracy) {
+bool checkPrime(unsigned long long num) {
+    //if (num < SMALL_NUMBER) {
+    //    return simplePrime(num);
+    //}    
+    return primeMillerRabin(num, DEFAULT_ACCARACY);
+}
+
+bool primeMillerRabin(unsigned long long num, int accaracy) {
     if ((num & 1) == 0) {
         // num is even
         return false;
     }
-    long long base2Power = factor2Power(num - 1); 
-    long long remainder = num >> base2Power; // should be odd
+    unsigned long long base2Power = factor2Power(num - 1); 
+    unsigned long long remainder = num >> base2Power; // should be odd
 
     std::random_device rd;  // a seed source for the random number engine
     std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
     std::uniform_int_distribution<> distrib(2, num - 2);
 
     for (int k = 0; k < accaracy; k++) {
-        long long base = distrib(gen);
+        unsigned long long base = distrib(gen);
         if (witnessMillerRabin(num, base2Power, remainder, base) == false) {
             std::cout << "witness " << base << std::endl;
             return false;
@@ -24,12 +31,12 @@ bool primeMillerRabin(long long num, int accaracy) {
 }
 
 
-bool witnessMillerRabin(long long num, long long base2Power, long long remainder, long long base) {
-    long long test = safeBasedPow(base, remainder, num);
+bool witnessMillerRabin(unsigned long long &num, unsigned long long &base2Power, unsigned long long &remainder, unsigned long long &base) {
+    unsigned long long test = safeBasedPow(base, remainder, num);
     if (test == 1 || test == num - 1) {
         return true;
     }
-    for (long long s = 0; s < base2Power; s++) {
+    for (unsigned long long s = 0; s < base2Power; s++) {
         test = (test * test) % num;
         if (test == 1) {
             return false;
@@ -41,16 +48,16 @@ bool witnessMillerRabin(long long num, long long base2Power, long long remainder
     return false;
 }
 
-long long safeBasedPow(long long base, long long pow, long long mod) {
-    long long result = 1;
-    for (long long i = 0; i < pow; i++) {
+unsigned long long safeBasedPow(unsigned long long &base, unsigned long long &pow, unsigned long long &mod) {
+    unsigned long long result = 1;
+    for (unsigned long long i = 0; i < pow; i++) {
         result = (result * base) % mod;
     }
     return result;
 }
 
-long long factor2Power(long long num) {
-    long long power = 0;
+unsigned long long factor2Power(unsigned long long num) {
+    unsigned long long power = 0;
     while ((num & 1) == 0) {
         num = num >> 1;
         power++;
--- primer.h
+++ primer.h
@@ -3,25 +3,29 @@
 #include <iostream>
 #include <random>
 
+
+const int DEFAULT_ACCARACY = 10; // 4 ^ -10 accaracy.
+
+
 /**
  * Check if the number is prime according to MillerRabin test.
  * This is a probalistic test, thus we need to choose acceracy.
  * Its accaracy is 4^-k, where k is the number of iterations.
  */
-bool primeMillerRabin(long long num, int accaracy);
+bool primeMillerRabin(unsigned long long num, int accaracy);
 
 
 /**
  * Factor the given num into 2^s * d
  */
-long long factor2Power(long long num);
+unsigned long long factor2Power(unsigned long long num);
 
 
 /**
  * Calculates (base ^ pow) % (mod), but instead of in one go, takes modulo at
  * each iteration of _ * base. this allows us to avoid overflows more easly.
  */
-long long safeBasedPow(long long base, long long pow, long long mod);
+unsigned long long safeBasedPow(unsigned long long &base, unsigned long long &pow, unsigned long long &mod);
 
 
 /**
@@ -29,5 +33,5 @@ long long safeBasedPow(long long base, long long pow, long long mod);
  * It is also passed base2Power and remainder. which are the decomposition of num
  * into base 2 powers. this is done to avoid recalculating
  */
-bool witnessMillerRabin(long long num, long long base2Power, long long remainder, long long base);
+bool witnessMillerRabin(unsigned long long &num, unsigned long long &base2Power, unsigned long long &remainder, unsigned long long &base);
 
